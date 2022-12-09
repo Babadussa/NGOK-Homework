1adc442c74a3ca2073299318f4156ef0b66be535
Babadussa <babadussa478@gmail.com>
2022-12-09 10:47:47 +0700

    ff

--- test.c
+++ test.c
@@ -4,11 +4,15 @@
 int main(void)
 {
     int i = 0;
+    int j = 0;
+    double tmp = 0;
     double mass1[SIZE];
     double mass2[SIZE];
     double mass3[SIZE];
     double sum[SIZE];
     double sred[SIZE];
+    int s = 0;
+    int m = 0;
     for(i = 0;i <= SIZE - 1;i++)
     {
         scanf("%lf",&mass1[i]);
@@ -46,12 +50,29 @@ int main(void)
     {
         printf("%lf ",sred[i]);
     }
-    for(i = 0;i <= SIZE - 1; i++)
+    printf("\n");
+    for(int i = 0,j = SIZE - 1 ;i < SIZE / 2; ++i , --j )
     {
-        if(mass1[i] %2 = 0)
-        {
-            s += 1;
-        }
+        tmp = mass1[i];
+        mass1[i] = mass1[j];
+        mass1[j] = tmp;
     }
-
+    for(i = 0;i < SIZE;i++)
+    {
+        printf("%lf ",mass1[i]); 
+    }
+    printf("\nОбъединение массивов в 1 массив \n");
+    int SIZE2 = SIZE + SIZE;
+    for(i = 0,j < SIZE; i < SIZE2 /2,j >= 0;i++,j--)
+    {
+        mass3[i] = mass1[j];
+    }
+    for(i < SIZE2,j = 0;i >= SIZE2 /2,j < SIZE;i--,j++)
+    {
+        mass3[i] = mass2[j];
+    }
+    for(i = 0;i < SIZE2 ; i++)
+    {
+        printf("%lf ",mass3[i]);
+    }
 }
